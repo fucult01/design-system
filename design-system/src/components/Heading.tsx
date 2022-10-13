112f7aa72460d013bc0353c753a24e877adf1bb9
Artur <fucult96@gmail.com>
2022-10-13 11:41:49 -0300

    improve(component): generic className as Heading Component props
    to make sure that the component that calls Heading component can style the heading

--- design-system/src/components/Heading.tsx
+++ design-system/src/components/Heading.tsx
@@ -1,17 +1,20 @@
 import { ReactNode } from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { clsx } from 'clsx';
+import { StringLiteral } from 'typescript';
 
 export interface HeadingProps {
   size?: 'sm' | 'md' | 'lg';
   children: ReactNode;
   asChild?: boolean;
+  className?: StringLiteral;
 }
 
 export default function Heading({
   size = 'md',
   children,
   asChild,
+  className,
 }: HeadingProps) {
   const Comp = asChild ? Slot : 'h2';
 
@@ -21,7 +24,7 @@ export default function Heading({
         'text-lg': size === 'sm',
         'text-xl': size === 'md',
         'text-2xl': size === 'lg',
-      })}
+      }, className)}
     >
       {children}
     </Comp>
