57f1e6d76d8a7676c9b4431ead6c520b9887e77c
Artur <fucult96@gmail.com>
2022-10-13 09:30:20 -0300

    improve(component): create TextInput option without icon

--- design-system/src/components/TextInput.tsx
+++ design-system/src/components/TextInput.tsx
@@ -8,8 +8,8 @@ export interface TextInputRootProps {
 function TextInputRoot(props: TextInputRootProps) {
   return (
     <div
-      className={`flex items-center gap-3 py-4 px-3 rounded bg-gray-800 w-full 
-    focus-within:ring-2 ring-cyan-300`}
+      className={`flex items-center gap-3 h-12 py-4 px-3 rounded bg-gray-800 
+      w-full focus-within:ring-2 ring-cyan-300`}
     >
       {props.children}
     </div>
