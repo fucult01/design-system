869a767e88bd2c023d8817f54f85afd4dc3abd99
Artur <fucult96@gmail.com>
2022-10-12 23:14:45 -0300

    feat(component): create Button component

--- design-system/tailwind.config.cjs
+++ design-system/tailwind.config.cjs
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.tsx',
-  ],
+  content: ["./src/**/*.tsx"],
   theme: {
     fontSize: {
       xs: 14,
@@ -10,23 +8,26 @@ module.exports = {
       md: 18,
       lg: 20,
       xl: 24,
-      '2xl': 32
+      "2xl": 32,
     },
     colors: {
-      'gray-900': '#121214',
-      'gray-800': '#202014',
-      'gray-400': '#7C7C8A',
-      'gray-200': '#C4C4CC',
-      'gray-100': '#E1E1E6',
+      black: "#000",
+      white: "#FFF",
 
-      'cyan-500': '#81D8F7',
-      'cyan-300': '#9BE1FB'
+      "gray-900": "#121214",
+      "gray-800": "#202014",
+      "gray-400": "#7C7C8A",
+      "gray-200": "#C4C4CC",
+      "gray-100": "#E1E1E6",
+
+      "cyan-500": "#81D8F7",
+      "cyan-300": "#9BE1FB",
     },
     extend: {
       fontFamily: {
-        sans: 'Inter, sans-serif'
-      }
+        sans: "Inter, sans-serif",
+      },
     },
   },
   plugins: [],
-}
+};
